ee1f39f252c89e8dc579156ad508a52f32681a74
kee_hoon <djfkfk12345@naver.com>
2022-10-19 11:37:48 +0900

    221019

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 import streamlit as st
 view=[100,150,30]
-st.write('# Youtube View')
+st.write('# Youtube Views')
 st.write('## raw')
 view
 st.write('## bar chart')
 st.bar_chart(view)
+
 import pandas as pd
 sview=pd.Series(view)
 sview
