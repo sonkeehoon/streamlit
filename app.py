cb726d44d49f984493b1cc5636a0da6c0e1890db
kee_hoon <djfkfk12345@naver.com>
2023-07-24 23:19:51 +0900

    230724

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 import streamlit as st
-view=[100,150,30]
-st.write('# Youtube Views')
+import pandas as pd
+
+f = open("./damage.txt",'r',encoding="utf-8")
+lines = f.readlines()
+view=[]
+for line in lines:
+    view.append(int(line))
+st.write('# 메이플 스탯공격력 변화')
 st.write('## raw')
 view
-st.write('## bar chart')
+st.write('## Chart')
+
 st.bar_chart(view)
 
-import pandas as pd
 sview=pd.Series(view)
 sview
